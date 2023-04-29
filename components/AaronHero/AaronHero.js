c78878ba6d78994fb1fbf7404713f89b58130f79
w3schools-test <aaronkaltman@gmail.com>
2023-04-28 21:07:56 -0400

    more hero shit

--- components/AaronHero/AaronHero.js
+++ components/AaronHero/AaronHero.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import Image from 'next/image';
 
 import styles from './AaronHero.module.scss';
 
 const AaronHero = () => {
     return (
         <section className={styles.hero}>
-            <div className="container">
-                <div className="row">
-                    <div className={`${styles.leftCol} col-md-6`}>
-                        {/* Removed Image component */}
+            <div className={styles.container}>
+                <div className={styles.row}>
+                    <div className={styles.leftCol}>
+                        <div className={styles.imageWrapper}>
+                            <Image
+                                src="https://bpheadlesst596.wpengine.com/wp-content/uploads/2023/04/phones-app-transparent-large.webp"
+                                alt="Fixdapp way of fixing cars"
+                                layout="responsive"
+                                width={400}
+                                height={350}
+                            />
+                        </div>
                     </div>
-                    <div className={`col-md-6`}>
+                    <div className={styles.rightCol}>
                         <div className={styles.content}>
-                            <h1 className={styles.heading}>Save money on vehicle repairs & car mechanic bills</h1>
+                            <h1 className={styles.heading}>Welcome to my website!</h1>
                             <p className={styles.text}>
-                                The FIXD sensor and free app tells you why your check engine light is on to help you save time and money
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, ante eu tristique pharetra, eros velit suscipit tortor, a porttitor ipsum turpis eu eros. Quisque vitae libero pharetra, congue sapien a, ultricies est. Fusce bibendum magna a risus eleifend interdum.
                             </p>
                             <button className={styles.button}>Learn More</button>
                         </div>
