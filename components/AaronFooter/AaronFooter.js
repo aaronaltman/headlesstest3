3014eab845efd8e52fb5c73afcb9070f28815dbe
w3schools-test <aaronkaltman@gmail.com>
2023-04-30 15:31:28 -0400

    Please work

--- components/AaronFooter/AaronFooter.js
+++ components/AaronFooter/AaronFooter.js
@@ -28,37 +28,37 @@ function AaronFooter() {
             </div>
             <div className={styles.column2}>
                 <h2 className={styles.local_h2}>About</h2>
-                <ul>
-                    <li>About FIXD</li>
-                    <li>Careers</li>
-                    <li>Press</li>
-                    <li>Reviews</li>
+                <ul className={styles.local_ul}>
+                    <li className={styles.local_li}>About FIXD</li>
+                    <li className={styles.local_li}>Careers</li>
+                    <li className={styles.local_li}>Press</li>
+                    <li className={styles.local_li}>Reviews</li>
                 </ul>
             </div>
             <div className={styles.column3}>
                 <h2 className={styles.local_h2}>Contact Us</h2>
-                <ul>
-                <li>(925) 854-1766</li>
-                <li>support@fixdapp.com</li>
-                <li>999 Peachtree St NE Suite 840, Atlanta, GA 30309</li>
+                <ul className={styles.local_ul}>
+                    <li className={styles.local_li}>(925) 854-1766</li>
+                    <li className={styles.local_li}>support@fixdapp.com</li>
+                    <li className={styles.local_li}>999 Peachtree St NE Suite 840, Atlanta, GA 30309</li>
                 </ul>
             </div>
             <div className={styles.column4}>
                 <h2 className={styles.local_h2}>Help</h2>
-                <ul>
-                    <li>Help Center</li>
-                    <li>Contact Support</li>
-                    <li>FIXD app and sensor FAQ</li>
-                    <li>Returns and warranty</li>
-                    <li>Manage subscription</li>
+                <ul className={styles.local_ul}>
+                    <li className={styles.local_li}>Help Center</li>
+                    <li className={styles.local_li}>Contact Support</li>
+                    <li className={styles.local_li}>FIXD app and sensor FAQ</li>
+                    <li className={styles.local_li}>Returns and warranty</li>
+                    <li className={styles.local_li}>Manage subscription</li>
                 </ul>
             </div>
             <div className={styles.column5}>
                 <h2 className={styles.local_h2}>Legal</h2>
-                <ul>
-                    <li>Terms of Use</li>
-                    <li>Privacy Policy</li>
-                    <li>Advertising Disclosure</li>
+                <ul className={styles.local_ul}>
+                    <li className={styles.local_li}>Terms of Use</li>
+                    <li className={styles.local_li}>Privacy Policy</li>
+                    <li className={styles.local_li}>Advertising Disclosure</li>
                 </ul>
             </div>
         </div>
