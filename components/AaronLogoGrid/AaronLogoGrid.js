2607b36ab2d292b7c389b21c95ab747a19778acf
w3schools-test <aaronkaltman@gmail.com>
2023-04-29 23:45:39 -0400

    Ok let's see what happens

--- components/AaronLogoGrid/AaronLogoGrid.js
+++ components/AaronLogoGrid/AaronLogoGrid.js
@@ -18,7 +18,6 @@ const AaronLogoGrid = () => {
         'https://bpheadlesst596.wpengine.com/wp-content/uploads/2023/04/acm-blueprint/media/365/ff21d357-e318-3c8e-8ff2-ca15dbd1fa76.jpg',
         'https://bpheadlesst596.wpengine.com/wp-content/uploads/2023/04/acm-blueprint/media/365/ff21d357-e318-3c8e-8ff2-ca15dbd1fa76.jpg',
         'https://bpheadlesst596.wpengine.com/wp-content/uploads/2023/04/acm-blueprint/media/365/ff21d357-e318-3c8e-8ff2-ca15dbd1fa76.jpg',
-        'https://bpheadlesst596.wpengine.com/wp-content/uploads/2023/04/acm-blueprint/media/365/ff21d357-e318-3c8e-8ff2-ca15dbd1fa76.jpg',
     ];
 
     return (
