c3a0a4e91473cd3ff26a9e9778e35f111e3eae9f
w3schools-test <aaronkaltman@gmail.com>
2023-04-30 22:44:32 -0400

    Core UI Footer

--- components/AaronFooter/AaronFooter.js
+++ components/AaronFooter/AaronFooter.js
@@ -4,14 +4,14 @@ import '@coreui/coreui/dist/css/coreui.min.css';
 
 function AaronFooter() {
     return (
-        <CFooter className="py-4" style={{ paddingTop: '40px', paddingBottom: '40px' }}>
+        <CFooter className="py-4" style={{ backgroundColor: '#f5f5f5', paddingTop: '40px', paddingBottom: '40px' }}>
             <CContainer>
                 <CRow>
                     <CCol col="3" md="3" className="mb-3 mb-md-0">
                         <CImage
                             fluid
                             src="https://bpheadlesst596.wpengine.com/wp-content/uploads/2023/04/fixdapp-logo.webp"
-                            style={{ width: '75%', float: 'left' }}
+                            style={{ width: '50%', float: 'left' }}
                         />
                     </CCol>
                     <CCol md="9">
