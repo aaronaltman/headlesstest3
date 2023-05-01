4580b56d79ce4aaaa149a997023760854088c5f8
w3schools-test <aaronkaltman@gmail.com>
2023-05-01 10:10:15 -0400

    let's try this

--- components/AaronFooter/AaronFooter.js
+++ components/AaronFooter/AaronFooter.js
@@ -6,31 +6,31 @@ import 'components/AaronFooter/AaronFooter.module.scss';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary,
 }));
+
 const AaronFooter = () => {
     return (
         <div className={'localcontainer'}>
-        <Grid container spacing={2}>
-            <Grid item xs={4}>
-                <Item>xs</Item>
+            <Grid container spacing={2}>
+                <Grid item xs={4}>
+                    <Item>xs</Item>
+                </Grid>
+                <Grid item xs={2}>
+                    <Item>xs=6</Item>
+                </Grid>
+                <Grid item xs={2}>
+                    <Item>xs</Item>
+                </Grid>
+                <Grid item xs={2}>
+                    <Item>xs</Item>
+                </Grid>
+                <Grid item xs={2}>
+                    <Item>xs</Item>
+                </Grid>
             </Grid>
-            <Grid item xs={2}>
-                <Item>xs=6</Item>
-            </Grid>
-            <Grid item xs={2}>
-                <Item>xs</Item>
-            </Grid>
-            <Grid item xs={2}>
-                <Item>xs</Item>
-            </Grid>
-            <Grid item xs={2}>
-                <Item>xs</Item>
-            </Grid>
-        </Grid>
         </div>
     );
 };
