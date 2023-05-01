dc3256343ce163e0d87181a21af43e8f6562c6bb
w3schools-test <aaronkaltman@gmail.com>
2023-04-30 22:01:47 -0400

    Core UI

--- components/AaronFooter/AaronFooter.js
+++ components/AaronFooter/AaronFooter.js
@@ -1,39 +1,27 @@
 import React from 'react';
-import { CFooter, CLink } from '@coreui/react';
-import '@coreui/coreui/dist/css/coreui.min.css';
+import { CContainer, CRow, CCol, CFooter } from '@coreui/react';
 import Image from 'next/image';
 
 function AaronFooter() {
     return (
         <CFooter>
-            <div className="row">
-                <div className="col-md-2">
-                    <Image
-                        src="https://bpheadlesst596.wpengine.com/wp-content/uploads/2023/04/fixdapp-logo.webp"
-                        alt="Logo"
-                        layout="responsive"
-                        width={150}
-                        height={50}
-                    />
-                </div>
-                <div className="col-md-2">
-                    <CLink href="#">Column 2</CLink>
-                </div>
-                <div className="col-md-2">
-                    <CLink href="#">Column 3</CLink>
-                </div>
-                <div className="col-md-2">
-                    <CLink href="#">Column 4</CLink>
-                </div>
-                <div className="col-md-2">
-                    <CLink href="#">Column 5</CLink>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-md-12">
-                    <p>&copy; 2023 creativeLabs. All rights reserved.</p>
-                </div>
-            </div>
+            <CContainer>
+                <CRow>
+                    <CCol sm="auto">
+                        <Image
+                            src="https://bpheadlesst596.wpengine.com/wp-content/uploads/2023/04/fixdapp-logo.webp"
+                            alt="Logo"
+                            layout="responsive"
+                            width={150}
+                            height={50}
+                        />
+                    </CCol>
+                    <CCol sm="auto">Column 2</CCol>
+                    <CCol sm="auto">Column 3</CCol>
+                    <CCol sm="auto">Column 4</CCol>
+                    <CCol sm="auto">Column 5</CCol>
+                </CRow>
+            </CContainer>
         </CFooter>
     );
 }
