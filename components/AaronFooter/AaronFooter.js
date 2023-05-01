7a40e14755d4022403165091e4a8ba0edada4f9b
w3schools-test <aaronkaltman@gmail.com>
2023-04-30 22:49:56 -0400

    Let's try this baby

--- components/AaronFooter/AaronFooter.js
+++ components/AaronFooter/AaronFooter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CFooter, CContainer, CRow, CCol, CImage, CLink, CListGroup, CListGroupItem } from '@coreui/react';
+import { CFooter, CContainer, CRow, CCol, CImage, CListGroup, CListGroupItem } from '@coreui/react';
 import '@coreui/coreui/dist/css/coreui.min.css';
 
 function AaronFooter() {
@@ -14,28 +14,40 @@ function AaronFooter() {
                             style={{ width: '50%', float: 'left' }}
                         />
                     </CCol>
-                    <CCol md="9">
-                        <CRow>
-                            <CCol col="3" md="3" className="mb-3 mb-md-0">
-                                <h2>ABOUT</h2>
-                                <CListGroup flush>
-                                    <CListGroupItem>Cras justo odio</CListGroupItem>
-                                    <CListGroupItem>Dapibus ac facilisis in</CListGroupItem>
-                                    <CListGroupItem>Morbi leo risus</CListGroupItem>
-                                    <CListGroupItem>Porta ac consectetur ac</CListGroupItem>
-                                    <CListGroupItem>Vestibulum at eros</CListGroupItem>
-                                </CListGroup>
-                            </CCol>
-                            <CCol col="3" md="3" className="mb-3 mb-md-0">
-                                <CLink href="#">Link 2</CLink>
-                            </CCol>
-                            <CCol col="3" md="3" className="mb-3 mb-md-0">
-                                <CLink href="#">Link 3</CLink>
-                            </CCol>
-                            <CCol col="3" md="3" className="mb-3 mb-md-0">
-                                <CLink href="#">Link 4</CLink>
-                            </CCol>
-                        </CRow>
+                    <CCol col="3" md="3" className="mb-3 mb-md-0">
+                        <h2>ABOUT</h2>
+                        <CListGroup flush>
+                            <CListGroupItem>About FIXD</CListGroupItem>
+                            <CListGroupItem>Careers</CListGroupItem>
+                            <CListGroupItem>Press</CListGroupItem>
+                            <CListGroupItem>Reviews</CListGroupItem>
+                        </CListGroup>
+                    </CCol>
+                    <CCol col="3" md="3" className="mb-3 mb-md-0">
+                        <h2>CONTACT US</h2>
+                        <CListGroup flush className="bg-light">
+                            <CListGroupItem>(925) 854-1766</CListGroupItem>
+                            <CListGroupItem>support@fixdapp.com</CListGroupItem>
+                            <CListGroupItem>999 Peachtree St NE Suite 840, Atlanta, GA 30309</CListGroupItem>
+                        </CListGroup>
+                    </CCol>
+                    <CCol col="3" md="3" className="mb-3 mb-md-0">
+                        <h2>HELP</h2>
+                        <CListGroup flush>
+                            <CListGroupItem>Help Center</CListGroupItem>
+                            <CListGroupItem>Contact Support</CListGroupItem>
+                            <CListGroupItem>FIXD app and sensor FAQ</CListGroupItem>
+                            <CListGroupItem>Returns and warranty</CListGroupItem>
+                            <CListGroupItem>Manage subscription</CListGroupItem>
+                        </CListGroup>
+                    </CCol>
+                    <CCol col="3" md="3" className="mb-3 mb-md-0">
+                        <h2>LEGAL</h2>
+                        <CListGroup flush>
+                            <CListGroupItem>Terms of Use</CListGroupItem>
+                            <CListGroupItem>Privacy Policy</CListGroupItem>
+                            <CListGroupItem>Advertising Disclosure</CListGroupItem>
+                        </CListGroup>
                     </CCol>
                 </CRow>
             </CContainer>
