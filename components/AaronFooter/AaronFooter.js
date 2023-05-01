c842fa89b91aad030f53f98b77c74c4f42a2d81e
w3schools-test <aaronkaltman@gmail.com>
2023-04-30 22:28:49 -0400

    Core UI Footer

--- components/AaronFooter/AaronFooter.js
+++ components/AaronFooter/AaronFooter.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { CFooter, CContainer, CRow, CCol, CImage } from '@coreui/react';
+import { CFooter, CContainer, CRow, CCol, CImage, CLink } from '@coreui/react';
 import '@coreui/coreui/dist/css/coreui.min.css';
 
 function AaronFooter() {
     return (
-        <CFooter className="py-4">
+        <CFooter className="py-4" style={{ paddingTop: '40px', paddingBottom: '40px' }}>
             <CContainer>
                 <CRow>
-                    <CCol col="12" md="3" className="mb-3 mb-md-0">
-                        <CImage fluid src="https://bpheadlesst596.wpengine.com/wp-content/uploads/2023/04/fixdapp-logo.webp" style={{ width: '50%', float: 'left' }} />
+                    <CCol col="3" md="3" className="mb-3 mb-md-0">
+                        <CImage
+                            fluid
+                            src="https://bpheadlesst596.wpengine.com/wp-content/uploads/2023/04/fixdapp-logo.webp"
+                            style={{ width: '50%', float: 'left' }}
+                        />
                     </CCol>
-                    <CCol col="6" md="3" className="mb-3 mb-md-0">Column 2</CCol>
-                    <CCol col="6" md="3" className="mb-3 mb-md-0">Column 3</CCol>
-                    <CCol col="6" md="3" className="mb-3 mb-md-0">Column 4</CCol>
+                    <CCol md="9">
+                        <CRow>
+                            <CCol col="3" md="3" className="mb-3 mb-md-0">
+                                <CLink href="#">Link 1</CLink>
+                            </CCol>
+                            <CCol col="3" md="3" className="mb-3 mb-md-0">
+                                <CLink href="#">Link 2</CLink>
+                            </CCol>
+                            <CCol col="3" md="3" className="mb-3 mb-md-0">
+                                <CLink href="#">Link 3</CLink>
+                            </CCol>
+                            <CCol col="3" md="3" className="mb-3 mb-md-0">
+                                <CLink href="#">Link 4</CLink>
+                            </CCol>
+                        </CRow>
+                    </CCol>
                 </CRow>
             </CContainer>
         </CFooter>
@@ -20,4 +37,5 @@ function AaronFooter() {
 }
 
 export default AaronFooter;
+
 
