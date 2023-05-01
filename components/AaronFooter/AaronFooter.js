8447dd20837bcb2279f8eb96e72d19bdee7233fb
w3schools-test <aaronkaltman@gmail.com>
2023-04-30 22:18:44 -0400

    Core UI Footer

--- components/AaronFooter/AaronFooter.js
+++ components/AaronFooter/AaronFooter.js
@@ -7,13 +7,12 @@ function AaronFooter() {
         <CFooter>
             <CContainer>
                 <CRow>
-                    <CCol xs="12" sm="4" className="mb-3 mb-md-0">
+                    <CCol col="12" md="3" className="mb-3 mb-md-0">
                         <CImage fluid src="https://bpheadlesst596.wpengine.com/wp-content/uploads/2023/04/fixdapp-logo.webp" style={{ width: '50%', float: 'left' }} />
                     </CCol>
-                    <CCol xs="6" sm="2" className="mb-3 mb-md-0">Column 2</CCol>
-                    <CCol xs="6" sm="2" className="mb-3 mb-md-0">Column 3</CCol>
-                    <CCol xs="6" sm="2" className="mb-3 mb-md-0">Column 4</CCol>
-                    <CCol xs="6" sm="2" className="mb-3 mb-md-0">Column 5</CCol>
+                    <CCol col="6" md="3" className="mb-3 mb-md-0">Column 2</CCol>
+                    <CCol col="6" md="3" className="mb-3 mb-md-0">Column 3</CCol>
+                    <CCol col="6" md="3" className="mb-3 mb-md-0">Column 4</CCol>
                 </CRow>
             </CContainer>
         </CFooter>
