1cbada7efc5245eade33a231dc612b5233c04523
w3schools-test <aaronkaltman@gmail.com>
2023-05-01 10:36:46 -0400

    will this work - who knows!

--- components/AaronFooter/AaronFooter.js
+++ components/AaronFooter/AaronFooter.js
@@ -1,29 +1,39 @@
 import React from 'react';
+import Paper from '@mui/material/Paper';
 import 'components/AaronFooter/AaronFooter.module.scss';
 import Grid from '@mui/material/Grid';
 
 
 const AaronFooter = () => {
     return (
-        <div className={'localcontainer'}>
-            <Grid container spacing={2}>
-                <Grid item xs={4}>
-                    <p>xs</p>
+        <Paper
+            sx={{
+                p: 2,
+                margin: 'auto',
+                maxWidth: 500,
+                flexGrow: 1,
+            }}
+        >
+            <div className={'localcontainer'}>
+                <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                        <p>xs</p>
+                    </Grid>
+                    <Grid item xs={2}>
+                        <p>xs=6</p>
+                    </Grid>
+                    <Grid item xs={2}>
+                        <p>xs</p>
+                    </Grid>
+                    <Grid item xs={2}>
+                        <p>xs</p>
+                    </Grid>
+                    <Grid item xs={2}>
+                        <p>xs</p>
+                    </Grid>
                 </Grid>
-                <Grid item xs={2}>
-                    <p>xs=6</p>
-                </Grid>
-                <Grid item xs={2}>
-                    <p>xs</p>
-                </Grid>
-                <Grid item xs={2}>
-                    <p>xs</p>
-                </Grid>
-                <Grid item xs={2}>
-                    <p>xs</p>
-                </Grid>
-            </Grid>
-        </div>
+            </div>
+        </Paper>
     );
 };
 
