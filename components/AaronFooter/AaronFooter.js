612861d2efe6a9536c52487b85615f3c13dd6bad
w3schools-test <aaronkaltman@gmail.com>
2023-04-30 23:52:30 -0400

    doing a little huh huh

--- components/AaronFooter/AaronFooter.js
+++ components/AaronFooter/AaronFooter.js
@@ -11,42 +11,42 @@ function AaronFooter() {
                         <CImage
                             fluid
                             src="https://bpheadlesst596.wpengine.com/wp-content/uploads/2023/04/fixdapp-logo.webp"
-                            style={{ width: '90%', float: 'left' }}
+                            style={{ width: '80%', float: 'left' }}
                         />
                     </CCol>
                     <CCol flex="1">
                         <h2 style={{ fontSize: '20px' }}>ABOUT</h2>
                         <CListGroup flush>
-                            <CListGroupItem>About FIXD</CListGroupItem>
-                            <CListGroupItem>Careers</CListGroupItem>
-                            <CListGroupItem>Press</CListGroupItem>
-                            <CListGroupItem>Reviews</CListGroupItem>
+                            <CListGroupItem style={{ backgroundColor: 'var(--bg-light)' }}>About FIXD</CListGroupItem>
+                            <CListGroupItem style={{ backgroundColor: 'var(--bg-light)' }}>Careers</CListGroupItem>
+                            <CListGroupItem style={{ backgroundColor: 'var(--bg-light)' }}>Press</CListGroupItem>
+                            <CListGroupItem style={{ backgroundColor: 'var(--bg-light)' }}>Reviews</CListGroupItem>
                         </CListGroup>
                     </CCol>
                     <CCol flex="1">
                         <h2 style={{ fontSize: '20px' }}>CONTACT US</h2>
                         <CListGroup flush className="bg-light">
-                            <CListGroupItem>(925) 854-1766</CListGroupItem>
-                            <CListGroupItem>support@fixdapp.com</CListGroupItem>
-                            <CListGroupItem>999 Peachtree St NE Suite 840, Atlanta, GA 30309</CListGroupItem>
+                            <CListGroupItem style={{ backgroundColor: 'var(--bg-light)' }}>(925) 854-1766</CListGroupItem>
+                            <CListGroupItem style={{ backgroundColor: 'var(--bg-light)' }}>support@fixdapp.com</CListGroupItem>
+                            <CListGroupItem style={{ backgroundColor: 'var(--bg-light)' }}>999 Peachtree St NE Suite 840, Atlanta, GA 30309</CListGroupItem>
                         </CListGroup>
                     </CCol>
                     <CCol flex="1">
                         <h2 style={{ fontSize: '20px' }}>HELP</h2>
                         <CListGroup flush>
-                            <CListGroupItem>Help Center</CListGroupItem>
-                            <CListGroupItem>Contact Support</CListGroupItem>
-                            <CListGroupItem>FIXD app and sensor FAQ</CListGroupItem>
-                            <CListGroupItem>Returns and warranty</CListGroupItem>
-                            <CListGroupItem>Manage subscription</CListGroupItem>
+                            <CListGroupItem style={{ backgroundColor: 'var(--bg-light)' }}>Help Center</CListGroupItem>
+                            <CListGroupItem style={{ backgroundColor: 'var(--bg-light)' }}>Contact Support</CListGroupItem>
+                            <CListGroupItem style={{ backgroundColor: 'var(--bg-light)' }}>FIXD app and sensor FAQ</CListGroupItem>
+                            <CListGroupItem style={{ backgroundColor: 'var(--bg-light)' }}>Returns and warranty</CListGroupItem>
+                            <CListGroupItem style={{ backgroundColor: 'var(--bg-light)' }}>Manage subscription</CListGroupItem>
                         </CListGroup>
                     </CCol>
                     <CCol flex="1">
                         <h2 style={{ fontSize: '20px' }}>LEGAL</h2>
                         <CListGroup flush>
-                            <CListGroupItem>Terms of Use</CListGroupItem>
-                            <CListGroupItem>Privacy Policy</CListGroupItem>
-                            <CListGroupItem>Advertising Disclosure</CListGroupItem>
+                            <CListGroupItem style={{ backgroundColor: 'var(--bg-light)' }}>Terms of Use</CListGroupItem>
+                            <CListGroupItem style={{ backgroundColor: 'var(--bg-light)' }}>Privacy Policy</CListGroupItem>
+                            <CListGroupItem style={{ backgroundColor: 'var(--bg-light)' }}>Advertising Disclosure</CListGroupItem>
                         </CListGroup>
                     </CCol>
                 </CRow>
