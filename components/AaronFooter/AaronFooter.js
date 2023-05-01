243f5c07d0ae87bf91ad472f3570bb40b3242a1c
w3schools-test <aaronkaltman@gmail.com>
2023-05-01 09:39:31 -0400

    let's see something interesting

--- components/AaronFooter/AaronFooter.js
+++ components/AaronFooter/AaronFooter.js
@@ -2,30 +2,26 @@ import React from 'react';
 import Grid from '@mui/material/Grid';
 import 'components/AaronFooter/AaronFooter.module.scss';
 
-function AaronFooter() {
+const AaronFooter = () => {
     return (
-        <div className="footer">
-            <div className="container">
-                <Grid container className="row">
-                    <Grid item xs={12} sm={3} className="col1">
-                        {/* Your column 1 content here */}
-                    </Grid>
-                    <Grid item xs={12} sm={3} className="col2">
-                        {/* Your column 2 content here */}
-                    </Grid>
-                    <Grid item xs={12} sm={3} className="col3">
-                        {/* Your column 3 content here */}
-                    </Grid>
-                    <Grid item xs={12} sm={3} className="col4">
-                        {/* Your column 4 content here */}
-                    </Grid>
-                    <Grid item xs={12} sm={3} className="col5">
-                        {/* Your column 5 content here */}
-                    </Grid>
-                </Grid>
-            </div>
-        </div>
+        <Grid container style={{ width: 1200 }}>
+            <Grid item xs={3} style={{ backgroundColor: 'red', height: 100 }}>
+                Column 1
+            </Grid>
+            <Grid item xs={3} style={{ backgroundColor: 'blue', height: 100 }}>
+                Column 2
+            </Grid>
+            <Grid item xs={3} style={{ backgroundColor: 'green', height: 100 }}>
+                Column 3
+            </Grid>
+            <Grid item xs={3} style={{ backgroundColor: 'orange', height: 100 }}>
+                Column 4
+            </Grid>
+            <Grid item xs={12} style={{ backgroundColor: 'pink', height: 100 }}>
+                Column 5
+            </Grid>
+        </Grid>
     );
-}
+};
 
 export default AaronFooter;
